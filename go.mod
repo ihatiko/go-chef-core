b9e08cebb0af8c62f009870e1828113fcf145ef0
Slava <verdark768@yandex.ru>
2024-12-29 14:19:24 +0300

    update

--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module github.com/ihatiko/go-chef-core
 go 1.23.4
 
 require (
+	github.com/ihatiko/go-chef-code-gen-utils v0.0.4
 	github.com/ihatiko/go-chef-modules-sdk v0.0.3
+	github.com/pelletier/go-toml/v2 v2.2.3
 	github.com/spf13/cobra v1.8.1
 )
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/mod v0.22.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,55 +1,103 @@
 package main
 
 import (
+	_ "embed"
 	"errors"
 	"fmt"
+	gochefcodegenutils "github.com/ihatiko/go-chef-code-gen-utils"
 	sdk "github.com/ihatiko/go-chef-modules-sdk"
+	"github.com/pelletier/go-toml/v2"
 	"github.com/spf13/cobra"
 	"io/fs"
+	"log/slog"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
-const fileName = "go-chef-core-modules.toml"
+const configFileName = "go-chef-core-modules.toml"
 const configDir = ".go-chef"
+const mainConfigName = "go-chef.toml"
 
 type Module struct {
 	Desc       string `toml:"desc"`
 	Path       string `toml:"path"`
 	Deprecated bool   `toml:"deprecated"`
 }
-type Modules map[string]Module
+type Config struct {
+	Modules map[string]Module `toml:"modules"`
+}
+type MainConfig struct {
+	Proxies []string `toml:"proxies"`
+}
+
+//go:embed config.toml
+var defaultConfig []byte
 
 func main() {
-	//dir, err := os.UserConfigDir()
-	//if err != nil {
-	//	slog.Error("Error getting user cache dir", slog.Any("err", err.Error()))
-	//	return
-	//}
-	//dirPath := filepath.Join(dir, configDir)
-	//if state, err := exists(dirPath); err != nil || state {
-	//	err = os.Mkdir(dirPath, os.ModePerm)
-	//	if err != nil {
-	//		slog.Error("Error creating user cache dir", slog.Any("err", err.Error()))
-	//		return
-	//	}
-	//}
-
-	//fmt.Println(dir, err)
+	cfg := getConfig()
+	dir, err := os.UserHomeDir()
+	if err != nil {
+		slog.Error("Error getting user cache dir", slog.Any("err", err))
+		return
+	}
+	configPath := filepath.Join(dir, configDir, configFileName)
 	module := sdk.NewModule()
 	module.AddCommands(
-		module.NewCommand("sandbox", func(cmd *cobra.Command, args []string) {
-			fmt.Println("sandbox test")
-		}),
 		module.NewDetailCommand("show-config", "Show configuration", "", func(cmd *cobra.Command, args []string) {
-			fmt.Println("TODO")
+			bytes, err := os.ReadFile(configPath)
+			if err != nil {
+				return
+			}
+			fmt.Println(string(bytes))
 		}),
 		module.NewDetailCommand("reset-config", "Reset configuration to base setup", "", func(cmd *cobra.Command, args []string) {
-			fmt.Println("TODO")
+			err := os.WriteFile(configPath, defaultConfig, os.ModePerm)
+			if err != nil {
+				return
+			}
+			slog.Info("reset config", slog.String("path", configPath))
 		}),
 		module.NewDetailCommand("config-path", "Show config path", "", func(cmd *cobra.Command, args []string) {
-			fmt.Println("TODO")
+			fmt.Println(configPath)
 		}),
 	)
+
+	mainConfigPath := filepath.Join(dir, configDir, mainConfigName)
+	if _, err := os.Stat(mainConfigPath); errors.Is(err, fs.ErrNotExist) {
+		slog.Error("mainConfigPath does not exist", slog.String("mainConfigPath", mainConfigPath))
+		return
+	}
+
+	mainConfigBytes, err := os.ReadFile(mainConfigPath)
+	if err != nil {
+		slog.Error("Error reading main config file", slog.String("mainConfigPath", mainConfigPath))
+		return
+	}
+	mainConfig := new(MainConfig)
+	err = toml.Unmarshal(mainConfigBytes, mainConfig)
+	if err != nil {
+		slog.Error("Error parsing main config file", slog.String("mainConfigPath", mainConfigPath), slog.Any("err", err))
+		return
+	}
+
+	updater := gochefcodegenutils.NewUpdater(mainConfig.Proxies)
+	composer := gochefcodegenutils.NewExecutor()
+	for key, md := range cfg.Modules {
+		cm := module.NewDetailCommand(key, md.Desc, "", func(cmd *cobra.Command, args []string) {
+			updater.AutoUpdate(md.Path)
+			splittedPath := strings.Split(md.Path, "/")
+			coreCommand := splittedPath[len(splittedPath)-1]
+			params := strings.Join(os.Args[1:], " ")
+			proxyCommand := fmt.Sprintf("%s %s", coreCommand, params)
+			result, err := composer.ExecDefaultCommand(proxyCommand)
+			if err != nil {
+				slog.Error("Error executing command: ", slog.Any("error", err), slog.String("command", params))
+			}
+			fmt.Println(result)
+		})
+		module.AddCommands(cm)
+	}
 	module.Run()
 }
 func exists(path string) (bool, error) {
@@ -61,4 +109,53 @@ func exists(path string) (bool, error) {
 		return false, nil
 	}
 	return false, err
+}
+
+func getConfig() *Config {
+	dir, err := os.UserHomeDir()
+	if err != nil {
+		slog.Error("Error getting user cache dir", slog.Any("err", err))
+		return nil
+	}
+	configPath := filepath.Join(dir, configDir, configFileName)
+	state, err := exists(configPath)
+	if err != nil {
+		slog.Error("Error checking if config file exists", slog.Any("err", err))
+		return nil
+	}
+	if !state {
+		dirPath := filepath.Join(dir, configDir)
+		dirState, err := exists(dirPath)
+		if err != nil {
+			slog.Error("Error checking if config file exists", slog.Any("err", err))
+			return nil
+		}
+		if !dirState {
+			slog.Info("Config dir does not exist, creating", slog.Any("dir", dirPath))
+			err := os.Mkdir(dirPath, os.ModePerm)
+			if err != nil {
+				slog.Error("Error creating config dir", slog.Any("err", err))
+				return nil
+			}
+		}
+		slog.Info("try create system config file")
+		err = os.WriteFile(configPath, defaultConfig, os.ModePerm)
+		if err != nil {
+			slog.Error("Error creating config file", slog.Any("err", err))
+			return nil
+		}
+		slog.Info("config file created", slog.Any("file", configPath))
+	}
+	cfgBytes, err := os.ReadFile(configPath)
+	if err != nil {
+		slog.Error("Error reading config file", slog.Any("err", err), slog.String("file", configPath))
+		return nil
+	}
+	config := new(Config)
+	err = toml.Unmarshal(cfgBytes, config)
+	if err != nil {
+		slog.Error("Error unmarshalling default config", slog.Any("error", err))
+		return nil
+	}
+	return config
 }
